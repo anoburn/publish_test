a55c0f64419fbdc2a4dd63c300713279ff799db1
Stephen <stephen.s@users.noreply.github.com>
2020-07-15 18:43:52 +0200

    test release version

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,9 +5,15 @@ from distutils.version import StrictVersion
 with open("version.txt", "r") as f:
 	version = f.readline()
 
-def test_version():
+def test_version_unique():
 	url = "https://test.pypi.org/pypi/example-pkg-sschaum-2/json"
 	data = json.load(urlopen(url))
 	versions = list(data["releases"].keys())
 
 	assert version not in versions
+
+def test_release_version():
+	with open("github_version.txt", "r") as f:
+		github_version = f.readline()
+	
+	assert version == github_version
